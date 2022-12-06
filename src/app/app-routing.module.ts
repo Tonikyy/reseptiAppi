298b547fe7cfebad3b5ea4988388388006af2b91
lauhynn <laura.hynninen@student.laurea.fi>
2022-12-06 10:33:14 +0200

    recipes sivut luotu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,15 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'recipes',
+    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
+  },
+  {
+    path: 'my-recipes',
+    loadChildren: () => import('./my-recipes/my-recipes.module').then( m => m.MyRecipesPageModule)
   }
+
 ];
 @NgModule({
   imports: [
